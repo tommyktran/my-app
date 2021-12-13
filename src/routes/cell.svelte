89a252ce2d9bbeb4c2523b2ea841b6bd735d8d61
Tommy <tommy.tran@ivsllc.com>
2021-12-13 13:37:52 -0500

    Initial

---
+++ src/routes/cell.svelte
@@ -0,0 +1,14 @@
+<script>
+    
+</script>
+
+<div></div>
+
+<style>
+    div {
+        min-width: 50px;
+        min-height: 20px;
+        background-color: gray;
+        border: 1px solid #333;
+    }
+</style>
---
+++ src/routes/table.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Cell from "./cell.svelte";
+    export let cellCount = [1, 2, 3, 4, 5];
+    export let rowCount = [1, 2, 3];
+</script>
+
+<div class="header row">
+    {#each cellCount as cell}
+        <Cell class="header"></Cell>
+    {/each}
+</div>
+{#each rowCount as row}
+    <div class="row">
+        {#each cellCount as cell}
+            <Cell></Cell>
+        {/each}
+    </div>
+{/each}
+
+<style>
+    .row {
+        display: flex;
+        flex-direction: row;
+    }
+    .header {
+        border-bottom: 1px solid black;
+    }
+    .header {
+        background-color: white;
+    }
+</style>
